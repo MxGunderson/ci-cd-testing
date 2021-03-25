19a748031fc9baf11a800f65295e8c4cf2a7e0f9
Matthew Gunderson <matthew.gunderson12@gmail.com>
2021-03-25 13:56:36 -0700

    added one button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonOne from './components/ButtonOne';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img data-testid='image' src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World<br />Minor Change Here</h1>
+        <h1>Hello World</h1>
+        <ButtonOne />
       </header>
     </div>
   );
---
+++ src/components/ButtonOne.js
@@ -0,0 +1,7 @@
+export default function ButtonOne() {
+    return(
+<div>
+    <button>Click me...</button>
+</div >
+    )
+}
